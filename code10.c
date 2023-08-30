f21f270cfdf373d7ee17677159f61b0a86967eb5
angkon2587 <143635355+angkon2587@users.noreply.github.com>
2023-08-30 22:05:10 +0600

    code3

---
+++ code10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int x,y;
+printf("Name: Israf Md Mahin\n ID:2102076\n ");
+printf("input the value of two number:");
+scanf("%d%d",&x,&y);
+printf("Number 1:%d\n",x);
+printf("Number 2:%d\n",y);
+printf("Sum: %d\n",x+y);
+return 0;
+}
